d1179df587cae14beac19afb3463fc135a0222ed
xiemeigongzi88 <43561060+xiemeigongzi88@users.noreply.github.com>
2019-11-21 18:33:43 -0600

    Add files via upload

---
+++ Project/Utilizing PySpark and MLlib to solve a Binary Classification problem/pyspark_3.py
@@ -0,0 +1,210 @@
+# Databricks notebook source
+
+
+# COMMAND ----------
+
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+
+sqlContext = SQLContext(sc)
+df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/FileStore/tables/pyspark_learning/test/bank.csv')
+
+# COMMAND ----------
+
+df.printSchema()
+
+# COMMAND ----------
+
+import pandas as pd
+pd.DataFrame(df.take(5), columns=df.columns).transpose()
+
+# COMMAND ----------
+
+import pandas as pd
+pd.DataFrame(df.take(5), columns=df.columns).transpose()
+
+# COMMAND ----------
+
+numeric_features = [t[0] for t in df.dtypes if t[1] == 'int']
+df.select(numeric_features).describe().toPandas().transpose()
+
+# COMMAND ----------
+
+numeric_data = df.select(numeric_features).toPandas()
+axs = pd.plotting.scatter_matrix(numeric_data, figsize=(8, 8));
+n = len(numeric_data.columns)
+for i in range(n):
+    v = axs[i, 0]
+    v.yaxis.label.set_rotation(0)
+    v.yaxis.label.set_ha('right')
+    v.set_yticks(())
+    h = axs[n-1, i]
+    h.xaxis.label.set_rotation(90)
+    h.set_xticks(())
+
+# COMMAND ----------
+
+df = df.select('age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'deposit')
+cols = df.columns
+df.printSchema()
+
+# COMMAND ----------
+
+# Preparing Data for Machine Learning
+
+# COMMAND ----------
+
+from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
+categoricalColumns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'poutcome']
+stages = []
+for categoricalCol in categoricalColumns:
+    stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
+    encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
+    stages += [stringIndexer, encoder]
+label_stringIdx = StringIndexer(inputCol = 'deposit', outputCol = 'label')
+stages += [label_stringIdx]
+numericCols = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
+assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
+assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+stages += [assembler]
+
+# COMMAND ----------
+
+from pyspark.ml import Pipeline
+pipeline = Pipeline(stages = stages)
+pipelineModel = pipeline.fit(df)
+df = pipelineModel.transform(df)
+selectedCols = ['label', 'features'] + cols
+df = df.select(selectedCols)
+df.printSchema()
+
+# COMMAND ----------
+
+pd.DataFrame(df.take(5), columns=df.columns).transpose()
+
+# COMMAND ----------
+
+train, test = df.randomSplit([0.7, 0.3], seed = 2018)
+print("Training Dataset Count: " + str(train.count()))
+print("Test Dataset Count: " + str(test.count()))
+
+# COMMAND ----------
+
+# Logistic Regression Model
+
+# COMMAND ----------
+
+from pyspark.ml.classification import LogisticRegression
+lr = LogisticRegression(featuresCol = 'features', labelCol = 'label', maxIter=10)
+lrModel = lr.fit(train)
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+#%matplotlib inline
+import numpy as np
+beta = np.sort(lrModel.coefficients)
+plt.plot(beta)
+plt.ylabel('Beta Coefficients')
+display()
+
+# COMMAND ----------
+
+trainingSummary = lrModel.summary
+roc = trainingSummary.roc.toPandas()
+plt.plot(roc['FPR'],roc['TPR'])
+plt.ylabel('False Positive Rate')
+plt.xlabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+print('Training set areaUnderROC: ' + str(trainingSummary.areaUnderROC))
+
+display()
+
+# COMMAND ----------
+
+pr = trainingSummary.pr.toPandas()
+plt.plot(pr['recall'],pr['precision'])
+plt.ylabel('Precision')
+plt.xlabel('Recall')
+display()
+
+# COMMAND ----------
+
+predictions = lrModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+
+# COMMAND ----------
+
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+evaluator = BinaryClassificationEvaluator()
+print('Test Area Under ROC', evaluator.evaluate(predictions))
+
+# COMMAND ----------
+
+# Decision Tree Classifier
+
+# COMMAND ----------
+
+from pyspark.ml.classification import DecisionTreeClassifier
+dt = DecisionTreeClassifier(featuresCol = 'features', labelCol = 'label', maxDepth = 3)
+dtModel = dt.fit(train)
+predictions = dtModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+
+# COMMAND ----------
+
+evaluator = BinaryClassificationEvaluator()
+print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+
+# COMMAND ----------
+
+# Random Forest Classifier
+
+# COMMAND ----------
+
+from pyspark.ml.classification import RandomForestClassifier
+rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'label')
+rfModel = rf.fit(train)
+predictions = rfModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+
+# COMMAND ----------
+
+evaluator = BinaryClassificationEvaluator()
+print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+
+# COMMAND ----------
+
+# Gradient-Boosted Tree Classifier
+
+# COMMAND ----------
+
+from pyspark.ml.classification import GBTClassifier
+gbt = GBTClassifier(maxIter=10)
+gbtModel = gbt.fit(train)
+predictions = gbtModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+
+# COMMAND ----------
+
+evaluator = BinaryClassificationEvaluator()
+print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+
+# COMMAND ----------
+
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+paramGrid = (ParamGridBuilder()
+             .addGrid(gbt.maxDepth, [2, 4, 6])
+             .addGrid(gbt.maxBins, [20, 60])
+             .addGrid(gbt.maxIter, [10, 20])
+             .build())
+cv = CrossValidator(estimator=gbt, estimatorParamMaps=paramGrid, evaluator=evaluator, numFolds=5)
+# Run cross validations.  This can take about 6 minutes since it is training over 20 trees!
+cvModel = cv.fit(train)
+predictions = cvModel.transform(test)
+evaluator.evaluate(predictions)
+
+# COMMAND ----------
+
+
